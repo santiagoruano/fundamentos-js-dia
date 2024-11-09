7d2998bf6e2f0dc90603417136823bc88c2c7364
Santiago Ruano <ivansantiagoruanoruiz3@gmail.com>
2024-11-08 21:38:22 -0500

    se agrega el archico Fundamentos

---
+++ Fundamentos/recursos/js/Main.js
@@ -0,0 +1,16 @@
+/*Metodo mas usado */
+
+document.getElementById("demo").innerHTML = "Hello Javascript";
+document.getElementById("demo").style.fontSize = "35px";
+
+/* SalidasIde Datos */
+window.alert(5+2);
+console.log(6+2);
+
+/* declaraciones */
+let a,b,c;
+a = 5;
+b = 6;
+c = a + b;
+window.alert(c);
+document.getElementById("demo").innerHTML = c ;
